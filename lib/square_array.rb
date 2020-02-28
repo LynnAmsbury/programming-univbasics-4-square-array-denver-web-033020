80a7b972e55d6ba60824c8af7a6f48547c338bc0
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-02-28 23:53:09 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,21 +1,21 @@
-# def square_array(array)
-#   # your code here
-#   counter = 0
-#   while numbers[count] do
-#     numbers[counter] = (numbers[counter] * numbers[counter])
-#     counter += 1
-#   end
-#   return numbers
-# end
-
 def square_array(array)
-  counter = 0;
-  while array[counter] do
-     array[counter] **= 2;
-     counter += 1;
+  # your code here
+  counter = 0
+  while numbers[count] do
+    numbers[counter] = (numbers[counter] * numbers[counter])
+    counter += 1
   end
-  return array
+  return numbers
 end
+
+# def square_array(array)
+#   counter = 0;
+#   while array[counter] do
+#      array[counter] **= 2;
+#      counter += 1;
+#   end
+#   return array
+# end
 
 # counter = 0
 # pets = ["Dog", "Cat", "Fish", "Bird", "Hamster"]
