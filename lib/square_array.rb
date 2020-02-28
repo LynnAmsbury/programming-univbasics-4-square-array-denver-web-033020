2c5e5e1fdd91bbfe8fc05f358c6783d7677f7105
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-02-28 23:46:56 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   counter = 0
   numbers = [1,2,3]
   while numbers[counter] do
-    numbers[counter] * numbers[counter]
+    numbers[counter] = (numbers[counter] * numbers[counter])
     counter += 1
   end
 end
